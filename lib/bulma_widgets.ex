2a7005c64ff895de536dac3c7501576bdf49ad12
Jaremy Creechley <creechley@gmail.com>
2020-03-21 14:12:12 -0600

    initial repo

---
+++ lib/bulma_widgets.ex
@@ -0,0 +1,18 @@
+defmodule BulmaWidgets do
+  @moduledoc """
+  Documentation for BulmaWidgets.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> BulmaWidgets.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/bulma_widgets_test.exs
@@ -0,0 +1,8 @@
+defmodule BulmaWidgetsTest do
+  use ExUnit.Case
+  doctest BulmaWidgets
+
+  test "greets the world" do
+    assert BulmaWidgets.hello() == :world
+  end
+end
